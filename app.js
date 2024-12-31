ac692108728d095fb390db8f0fa7862bb0dd05c1
Hansraj meena <officialhansraj1@gmai.com>
2024-12-31 13:50:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+menuIcon.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
